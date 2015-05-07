ee1e4b6e281f36b42fa2bd7b0b4834351c1c61d9
Maruchi Kim <maruchi@Maruchis-Automatic-Macbook.local>
2015-05-06 22:53:34 -0700

    removed fullscreen button on player and added close button

--- client/views/twocents/video.js
+++ client/views/twocents/video.js
@@ -1,10 +1,53 @@
+document.addEventListener("fullscreenchange", FShandler);
+document.addEventListener("webkitfullscreenchange", FShandler);
+document.addEventListener("mozfullscreenchange", FShandler);
+document.addEventListener("MSFullscreenChange", FShandler);
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
+// Find the right method, call on correct element
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+function exitVideo() {
+  iframe = document.getElementById("iframe");
+  player.pauseVideo();
+  exitFullscreen();
+  $('iframe#player').css("z-index", "-10");
+  $('#close-button').css("z-index", "-10");
+}
+
 //Youtube object to change the state of the splash page's background based on whether or not the video is playing.
 onYouTubeIframeAPIReady = function () {
     player = new YT.Player("player", {
         height: "405", 
         width: "720", 
         videoId: "wRtLMYtwMhE", 
-        playerVars: {'color': "white", 'showinfo': 0},
+        playerVars:{'color': "red", 
+                    'showinfo': 0, 
+                    'theme': 'light', 
+                    'rel': 0, 
+                    'playsinline': 1, 
+                    'modestbranding': 1,
+                    'fs': 0,
+                    'autohide': 1},
         // Events like ready, state change, 
         events: {
             onReady: function (event) {
@@ -14,11 +57,6 @@ onYouTubeIframeAPIReady = function () {
 
             //Set played variable to true if video is playing, false otherwise
             onStateChange: function(event) {
-              if (player.getPlayerState() == 1) {
-                Session.set("played", true);
-              } else {
-                Session.set("played", false);
-              }
             }
         }
     });
@@ -27,12 +65,25 @@ onYouTubeIframeAPIReady = function () {
 //Load Youtube
 YT.load();
 
-
 Template.video.events({
-  'click #video-button': function(event){
-    console.log("test");
+  'click #video-button': function(event) {
     player.playVideo();
     $('iframe#player').css("z-index", "1000");
-  }
+    $('#close-button').css("z-index", "3147483647");
+    iframe = document.getElementById("player");
+    var requestFullScreen = iframe.requestFullScreen || iframe.mozRequestFullScreen || iframe.webkitRequestFullScreen;
+    if (requestFullScreen) {
+      launchIntoFullscreen(iframe);
+    }
+  },
 
+  'click #close-button': function(event) {
+    exitVideo();
+  }
 });
+
+function FShandler() {
+  if (!(document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement)) {
+    exitVideo();
+  } 
+}
